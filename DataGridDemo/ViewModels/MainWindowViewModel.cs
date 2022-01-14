d606a4eb406aa10dca0e1b422927a4804a3158d7
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-01-14 08:40:20 +0100

    Update MainWindowViewModel.cs

--- DataGridDemo/ViewModels/MainWindowViewModel.cs
+++ DataGridDemo/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace DataGridDemo.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
     }
 }
